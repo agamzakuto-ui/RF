4127ea9d3df3275171a3c8ea288a57a351b783d6
mendi <mendi3266@gmail.com>
2026-01-07 19:01:42 +0200

    separate pages to reciever and transmitter

--- apps/front_end/src/App.tsx
+++ apps/front_end/src/App.tsx
@@ -1,24 +1,24 @@
-
-import './App.css'
-import { FileUpload } from "./components/FileUpload";
-import SweepingHeatmap from './components/SweepingHeatmap'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
+import Receiver from "./pages/Receiver";
+import Transmitter from "./pages/Transmitter";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>LightningChart JS Sweeping Heatmap Demo</h1>
-        <p>Real-time spectrogram with {1000 * 250} data points visible</p>
-        <FileUpload />
-      </header>
-      
-      <main>
-        <div className="chart-container">
-          <SweepingHeatmap width={1000} height={600} />
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header"></header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Navigate to="/receiver" replace />} />
+            <Route path="/receiver" element={<Receiver />} />
+            <Route path="/transmitter" element={<Transmitter />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
