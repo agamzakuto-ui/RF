c1515c9664470bbc4574c6310e09c22473704e4a
sarahmax <sarahmax135@gmail.com>
2026-01-07 20:27:24 +0200

    fix conflict
    Signed-off-by: sarahmax <sarahmax135@gmail.com>

--- apps/front_end/src/App.tsx
+++ apps/front_end/src/App.tsx
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import Receiver from "./pages/Receiver";
 import Transmitter from "./pages/Transmitter";
+import Spectograma from "./components/spectograma/spectograma";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header"></header>
-=======
-
-import './App.css'
-import FileUploadButton from './components/inputFile'
-import Spectograma from './components/spectograma/spectograma'
-import SweepingHeatmap from './components/SweepingHeatmap'
-
-const App = () => {
-  return (
-      <Spectograma/>
-  )
-}
->>>>>>> StyleSpect
 
         <main>
           <Routes>
